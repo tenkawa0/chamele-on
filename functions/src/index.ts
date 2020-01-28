8ba8e86e199fd088c71d92b7b0df1e620a562be7
Yosuke Sogo <tenriver.ocean0@gmail.com>
2020-01-28 22:16:51 +0900

    テスト用HTTP関数を実装する
    HTTPリクエストに対して、administratorsコレクションの全ドキュメントを応答するテスト関数を作成。

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,17 @@
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
+import { collectionName } from './services/chamele-on/constants';
+
+admin.initializeApp();
+
+export const administrators = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (req, res) => {
+    const snap = await admin
+      .firestore()
+      .collection(collectionName.administrators)
+      .get();
+    const data = snap.docs.map(doc => doc.data());
+    res.send({ data });
+  });
