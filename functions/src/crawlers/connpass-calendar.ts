5d239f9aa13319d815137be69b79329473573616
Yosuke Sogo <tenriver.ocean0@gmail.com>
2020-02-02 23:04:13 +0900

    connpass用クローラ(プロト版)を作成 #7

--- functions/src/crawlers/connpass-calendar.ts
+++ functions/src/crawlers/connpass-calendar.ts
@@ -11,19 +11,118 @@ export const feedCalendar = async (page: puppeteer.Page) => {
   const memos: FeedMemo[] = [];
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const weeks = await page.$$(
-    '#contents > div.clearfix > div.main_area.mt_20 > table > tbody > tr',
+  const calendar = await page.$(
+    '#contents > div.clearfix > div.main_area.mt_20 > table > tbody',
   );
 
-  for await (const week of weeks) {
-    const days = await week.$$('td');
-    for await (const day of days) {
-      const items = await day.$$('.events > ul > li');
-      for await (const item of items) {
-        const memo = { ...blankFeedMemo };
-        memo.title = await item.$eval('span', e => e.textContent);
-        memos.push(memo);
+  if (calendar) {
+    // カレンダーに表示されているイベントのURLを全て取得
+    const hrefs = await calendar.$$eval('a', elements =>
+      elements.map(element => element.getAttribute('href')),
+    );
+
+    // イベント詳細ページから情報を抽出
+    for await (const href of hrefs) {
+      const memo = { ...blankFeedMemo };
+      if (href) {
+        await page.goto(href, {
+          waitUntil: 'domcontentloaded',
+        });
       }
+
+      memo.title = await page.title();
+      memo.url = href;
+
+      const subTitleHandle = await page.$('.event_subtitle');
+      if (subTitleHandle) {
+        const subTitle = await (
+          await subTitleHandle.getProperty('innerText')
+        ).jsonValue();
+        memo.subTitle = typeof subTitle === 'string' ? subTitle : '';
+      }
+
+      const thumbnaliHandle = await page.$('.event_header_area > a');
+      if (thumbnaliHandle) {
+        memo.thumbnail = await thumbnaliHandle.evaluate(e =>
+          e.getAttribute('href'),
+        );
+      }
+
+      const groupHandle = await page.$('.group_title > a');
+      if (groupHandle) {
+        const group = await (
+          await groupHandle.getProperty('innerText')
+        ).jsonValue();
+        memo.group = typeof group === 'string' ? group : '';
+        memo.groupUrl = await groupHandle.evaluate(e => e.getAttribute('href'));
+      }
+
+      const addressHandle = await page.$('.adr');
+      if (addressHandle) {
+        const address = await (
+          await addressHandle.getProperty('innerText')
+        ).jsonValue();
+        memo.address = typeof address === 'string' ? address : '';
+      }
+
+      const placeHandle = await page.$('.place_name');
+      if (placeHandle) {
+        const place = await (
+          await placeHandle.getProperty('innerText')
+        ).jsonValue();
+        memo.place = typeof place === 'string' ? place : '';
+      }
+
+      const dateHandle = await page.$('.dtstart > .ymd');
+      if (dateHandle) {
+        const date = await (
+          await dateHandle.getProperty('innerText')
+        ).jsonValue();
+        memo.date = typeof date === 'string' ? date : '';
+      }
+
+      const startTimeHandle = await page.$('.dtstart > .hi');
+      if (startTimeHandle) {
+        const startTime = await (
+          await startTimeHandle.getProperty('innerText')
+        ).jsonValue();
+        memo.startTime = typeof startTime === 'string' ? startTime : '';
+      }
+
+      const entryDurationHandle = await page.$('.event_entry_area > .period');
+      if (entryDurationHandle) {
+        const entryDuration = await (
+          await entryDurationHandle.getProperty('innerText')
+        ).jsonValue();
+        const entryDurationString =
+          typeof entryDuration === 'string' ? entryDuration : '';
+        if (entryDurationString) {
+          let entryDurationArray = entryDurationString.split(/\n/);
+          entryDurationArray = entryDurationArray.slice(1);
+          [memo.entryStart, memo.entryClose] = entryDurationArray;
+        }
+      }
+
+      const statusHandle = await page.$(
+        '.event_schedule_area > div > .label_status_event',
+      );
+      if (statusHandle) {
+        memo.status = await statusHandle.evaluate(e => e.textContent);
+      }
+
+      const hashtagHandle = await page.$('.label_hashtag > a');
+      if (hashtagHandle) {
+        const hashtag = await (
+          await hashtagHandle.getProperty('innerText')
+        ).jsonValue();
+        memo.hashtag = typeof hashtag === 'string' ? hashtag : '';
+        memo.hashtagUrl = await hashtagHandle.evaluate(e =>
+          e.getAttribute('href'),
+        );
+      }
+      memo.administrator = 'connpass';
+
+      memos.push(memo);
     }
   }
 
--- functions/src/services/chamele-on/models/feed-memo.ts
+++ functions/src/services/chamele-on/models/feed-memo.ts
@@ -3,12 +3,42 @@ import { firestore } from 'firebase/app';
 export type FeedMemo = {
   id?: string;
   title: string | null;
+  subTitle: string | null;
+  url: string | null;
+  thumbnail: string | null;
+  group: string | null;
+  groupUrl: string | null;
+  place: string | null;
+  address: string | null;
+  date: string | null;
+  startTime: string | null;
+  hashtag: string | null;
+  hashtagUrl: string | null;
+  entryStart: string | null;
+  entryClose: string | null;
+  status: string | null;
+  administrator: string | null;
   fetchedAt: firestore.Timestamp | null;
   createdAt: firestore.Timestamp | null;
 };
 
 export const blankFeedMemo: FeedMemo = {
   title: null,
+  subTitle: null,
+  url: null,
+  thumbnail: null,
+  group: null,
+  groupUrl: null,
+  place: null,
+  address: null,
+  date: null,
+  startTime: null,
+  hashtag: null,
+  hashtagUrl: null,
+  entryStart: null,
+  entryClose: null,
+  status: null,
+  administrator: null,
   fetchedAt: null,
   createdAt: null,
 };
