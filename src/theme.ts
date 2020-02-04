e3478f1595ec1f2aa4893694ddbae28fec3dd56b
Yosuke Sogo <tenriver.ocean0@gmail.com>
2020-02-05 00:52:54 +0900

    firebase用のcontextsを作成する

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+const chameleonTheme = {
+  color: {
+    primary: '#68e1e3',
+    secondary: '#ff6394',
+    tertiary: '#84ca66',
+    link: '#18c2c8',
+    white: '#ffffff',
+    black: '#1e1e2a',
+    gray: '#808080',
+    lightGray: '#979797',
+    indigo: '#5cb2cc',
+    ultraLightGray: '#e4e4e4',
+  },
+};
+
+export default chameleonTheme;
