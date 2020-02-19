2eefc13616df9d0cff122e54957ccfd3cb2fd17c
Yosuke Sogo <tenriver.ocean0@gmail.com>
2020-02-20 01:00:54 +0900

    2ヶ月分のデータを取得するように変更 #16

--- functions/src/crawlers/connpass.ts
+++ functions/src/crawlers/connpass.ts
@@ -9,76 +9,90 @@ export const feedConnpass = async (page: puppeteer.Page) => {
   await page.goto(url, { waitUntil: 'domcontentloaded' });
 
   const memos: FeedMemo[] = [];
+  const hrefs: string[] = [];
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const calendar = await page.$(
-    '#contents > div.clearfix > div.main_area.mt_20 > table > tbody',
-  );
-
-  if (calendar) {
-    // カレンダーに表示されているイベントのURLを全て取得
-    const hrefs = await calendar.$$eval('a', elements =>
-      elements.map(element => element.getAttribute('href')),
+  for await (const _ of [0, 1]) {
+    const calendar = await page.$(
+      '#contents > div.clearfix > div.main_area.mt_20 > table > tbody',
     );
 
-    // イベント詳細ページから情報を抽出
-    for await (const href of hrefs) {
-      const memo = { ...blankFeedMemo };
-      if (href) {
-        await page.goto(href, {
-          waitUntil: 'domcontentloaded',
-        });
-        const eventId = href.match(/\d+/)?.toString();
-        memo.eventId = `connpass:${eventId}`;
+    if (calendar) {
+      // カレンダーに表示されているイベントのURLを全て取得
+      const hrefList = await calendar.$$eval('a', elements =>
+        elements.map(element => element.getAttribute('href')),
+      );
+
+      for (const href of hrefList) {
+        if (href) hrefs.push(href);
       }
-
-      memo.title = await page.title();
-      memo.url = href;
-
-      const subTitleHandle = await page.$('.event_subtitle');
-      if (subTitleHandle) {
-        const subTitle = await (
-          await subTitleHandle.getProperty('innerText')
-        ).jsonValue();
-        memo.subTitle = typeof subTitle === 'string' ? subTitle : '';
-      }
-
-      const thumbnaliHandle = await page.$('.event_header_area > a');
-      if (thumbnaliHandle) {
-        memo.thumbnail = await thumbnaliHandle.evaluate(e =>
-          e.getAttribute('href'),
-        );
-      }
-
-      const addressHandle = await page.$('.adr');
-      if (addressHandle) {
-        const address = await (
-          await addressHandle.getProperty('innerText')
-        ).jsonValue();
-        memo.address = typeof address === 'string' ? address : '';
-      }
-
-      const placeHandle = await page.$('.place_name');
-      if (placeHandle) {
-        const place = await (
-          await placeHandle.getProperty('innerText')
-        ).jsonValue();
-        memo.place = typeof place === 'string' ? place : '';
-      }
-
-      const dateHandle = await page.$('.dtstart > .ymd');
-      if (dateHandle) {
-        const date = await (
-          await dateHandle.getProperty('innerText')
-        ).jsonValue();
-        memo.date = typeof date === 'string' ? date : '';
-      }
-
-      memo.administrator = 'connpass';
-      memo.prefecture = 'hyogo';
-
-      memos.push(memo);
     }
+
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click(
+        '#contents > div.clearfix > div.mb_15 > span > ul > li:nth-child(4) > a',
+      ),
+    ]);
+  }
+
+  // イベント詳細ページから情報を抽出
+  for await (const href of hrefs) {
+    const memo = { ...blankFeedMemo };
+    if (href) {
+      await page.goto(href, {
+        waitUntil: 'domcontentloaded',
+      });
+      const eventId = href.match(/\d+/)?.toString();
+      memo.eventId = `connpass:${eventId}`;
+    }
+
+    memo.title = await page.title();
+    memo.url = href;
+
+    const subTitleHandle = await page.$('.event_subtitle');
+    if (subTitleHandle) {
+      const subTitle = await (
+        await subTitleHandle.getProperty('innerText')
+      ).jsonValue();
+      memo.subTitle = typeof subTitle === 'string' ? subTitle : '';
+    }
+
+    const thumbnaliHandle = await page.$('.event_header_area > a');
+    if (thumbnaliHandle) {
+      memo.thumbnail = await thumbnaliHandle.evaluate(e =>
+        e.getAttribute('href'),
+      );
+    }
+
+    const addressHandle = await page.$('.adr');
+    if (addressHandle) {
+      const address = await (
+        await addressHandle.getProperty('innerText')
+      ).jsonValue();
+      memo.address = typeof address === 'string' ? address : '';
+    }
+
+    const placeHandle = await page.$('.place_name');
+    if (placeHandle) {
+      const place = await (
+        await placeHandle.getProperty('innerText')
+      ).jsonValue();
+      memo.place = typeof place === 'string' ? place : '';
+    }
+
+    const dateHandle = await page.$('.dtstart > .ymd');
+    if (dateHandle) {
+      const date = await (
+        await dateHandle.getProperty('innerText')
+      ).jsonValue();
+      memo.date = typeof date === 'string' ? date : '';
+    }
+
+    memo.administrator = 'connpass';
+    memo.prefecture = 'hyogo';
+
+    memos.push(memo);
   }
 
   return memos;
