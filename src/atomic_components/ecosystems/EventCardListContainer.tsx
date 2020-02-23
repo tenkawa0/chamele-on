100fd02b8a25c4c8efb2a72e31273806c730fbec
Yosuke Sogo <tenriver.ocean0@gmail.com>
2020-02-23 23:40:28 +0900

    タブ表示を実装 #18

--- src/atomic_components/ecosystems/EventCardListContainer.tsx
+++ src/atomic_components/ecosystems/EventCardListContainer.tsx
@@ -1,12 +1,47 @@
 import React from 'react';
 
+import { TabPaneProps } from 'semantic-ui-react/dist/commonjs/modules/Tab/TabPane';
+import { SemanticShorthandItem } from 'semantic-ui-react/dist/commonjs/generic.d';
+import { Event } from 'service/chamele-on/model/event';
+import { Tab } from 'semantic-ui-react';
 import EventCardList from '../organisms/List/EventCardList';
+import EventTab from '../organisms/Tab/EventTab';
 import useEvents from '../../hooks/use-events';
 
 const EventCardListContainer: React.FC = () => {
   const { events } = useEvents();
 
-  return <EventCardList events={events} />;
+  const osakaEvents: Event[] = events.filter(
+    event => event.prefecture === 'osaka',
+  );
+  const hyogoEvents: Event[] = events.filter(
+    event => event.prefecture === 'hyogo',
+  );
+
+  const panes: {
+    pane: SemanticShorthandItem<TabPaneProps>;
+    menuItem: string;
+  }[] = [];
+
+  panes.push({
+    pane: (
+      <Tab.Pane key="hyogo">
+        <EventCardList events={hyogoEvents} />
+      </Tab.Pane>
+    ),
+    menuItem: 'hyogo',
+  });
+
+  panes.push({
+    pane: (
+      <Tab.Pane key="osaka">
+        <EventCardList events={osakaEvents} />{' '}
+      </Tab.Pane>
+    ),
+    menuItem: 'osaka',
+  });
+
+  return <EventTab panes={panes} />;
 };
 
 export default EventCardListContainer;
